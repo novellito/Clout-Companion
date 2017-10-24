283d1edee3c22c6b5d7e840d97d5d5631f263383
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2017-10-23 23:26:41 -0700

    first commit & added calc ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { ExtrasComponent } from './components/extras/extras.component';
+
+const appRoutes: Routes = [ 
+  {path: '', component: AppComponent},
+  {path: 'calculator', component: CalculatorComponent},
+  {path: 'resources', component: ResourcesComponent},
+  {path: 'extras', component: ExtrasComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CalculatorComponent,
+    ResourcesComponent,
+    ExtrasComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
