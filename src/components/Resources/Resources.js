b136910ed9a96ac4e97256f79acd1e0b1c144ce0
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-05-17 00:07:18 -0700

    setup preliminary resource page

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import AppNavbar from '../AppNavbar/AppNavbar';
+import { Collapsible, CollapsibleItem } from 'react-materialize';
+import CoppingGuide from './CoppingGuides';
 const Resources = () => {
   return (
     <div>
       <AppNavbar />
-      <h1>Hello</h1>
+      <div className="row">
+        <div className="col s12 m10 l6 offset-l3 offset-m1 ">
+          <h1 className="comp-title">Resources</h1>
+          <Collapsible>
+            <CollapsibleItem header="Copping Guides">
+              Item1
+              <CoppingGuide />
+            </CollapsibleItem>
+            <CollapsibleItem header="Where to buy/sell">
+              Item1
+              <CoppingGuide />
+            </CollapsibleItem>
+            <CollapsibleItem header="Bots">
+              Item1
+              <CoppingGuide />
+            </CollapsibleItem>
+            <CollapsibleItem header="Proxies & Servers">
+              Item1
+              <CoppingGuide />
+            </CollapsibleItem>
+            <CollapsibleItem header="Shipping Resources">
+              Item1
+              <CoppingGuide />
+            </CollapsibleItem>
+            <CollapsibleItem header="Slang">
+              Item1
+              <CoppingGuide />
+            </CollapsibleItem>
+          </Collapsible>
+        </div>
+      </div>
     </div>
   );
 };
