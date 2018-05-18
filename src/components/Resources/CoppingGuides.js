f120aa6b532b92b63531b5b476a5d830b97bc754
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-05-18 13:54:02 -0700

    finish copping guide section

--- src/components/Resources/CoppingGuides.js
+++ src/components/Resources/CoppingGuides.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const CoppingGuides = () => {
-  return <div>cop</div>;
+  return (
+    <Fragment>
+      <p className="section-title">Multiple People Jig</p>
+      <a
+        href="https://www.youtube.com/channel/UCN8OxuBrKg6YJ-Kv4UB5Hjg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        (Credits to Keith Adam)
+      </a>
+      <div className="vid-container">
+        <iframe
+          width="460"
+          height="315"
+          className="jig1"
+          src="https://www.youtube.com/embed/e9nzZRzkNSE?showinfo=0"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          title="Multiple People Jig"
+          allowFullScreen
+        />
+      </div>
+      <p className="section-title">Copping Manual</p>
+      <p>
+        There's not much to be said about copping manual. It's all about
+        practice and being as fast as possible. Some useful tools to give you an
+        edge include using your browsers autofill feature or extensions
+        like&nbsp;
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://chrome.google.com/webstore/detail/autofill/nlmmgnhgdeffjkdckmikfpnddkbbfkkk?hl=en"
+        >
+          autofill
+        </a>{' '}
+        and
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://chrome.google.com/webstore/detail/autofill-for-chrome-by-fi/ojhegjfmbbpahdggoekcbmejnifimeca?hl=en"
+        >
+          &nbsp;fillr
+        </a>
+      </p>
+      <p>
+        Here's a guide by&nbsp;
+        <a
+          href="https://www.youtube.com/channel/UCS9mgvjxQN-cEXN4b8VJUWg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Chop Suey&nbsp;
+        </a>
+        on how to set up autofill (the process applies for other sites)
+      </p>
+      <div className="vid-container">
+        <iframe
+          width="460"
+          height="315"
+          className="jig1"
+          src="https://www.youtube.com/embed/zOPjFCR2nb4?showinfo=0"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          title="Autofill guide"
+          allowFullScreen
+        />
+      </div>
+    </Fragment>
+  );
 };
 
 export default CoppingGuides;
--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -3,6 +3,8 @@ import AppNavbar from '../AppNavbar/AppNavbar';
 import { Collapsible, CollapsibleItem } from 'react-materialize';
 import CoppingGuide from './CoppingGuides';
 import RowContainer from '../../hoc/rowContainer';
+import './Resources.css';
+
 const Resources = () => {
   return (
     <div>
@@ -11,28 +13,22 @@ const Resources = () => {
         <h1 className="comp-title">Resources</h1>
         <Collapsible>
           <CollapsibleItem header="Copping Guides">
-            Item1
             <CoppingGuide />
           </CollapsibleItem>
           <CollapsibleItem header="Where to buy/sell">
-            Item1
-            <CoppingGuide />
+            {/* <CoppingGuide /> */}
           </CollapsibleItem>
           <CollapsibleItem header="Bots">
-            Item1
-            <CoppingGuide />
+            {/* <CoppingGuide /> */}
           </CollapsibleItem>
           <CollapsibleItem header="Proxies & Servers">
-            Item1
-            <CoppingGuide />
+            {/* <CoppingGuide /> */}
           </CollapsibleItem>
           <CollapsibleItem header="Shipping Resources">
-            Item1
-            <CoppingGuide />
+            {/* <CoppingGuide /> */}
           </CollapsibleItem>
           <CollapsibleItem header="Slang">
-            Item1
-            <CoppingGuide />
+            {/* <CoppingGuide /> */}
           </CollapsibleItem>
         </Collapsible>
       </RowContainer>
