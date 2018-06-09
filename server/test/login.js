c26a66f6b2c9091baecabdeb63d2781795148508
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-06-09 10:27:18 -0700

    add test cases for middleware functions

--- server/test/login.js
+++ server/test/login.js
@@ -1,5 +1,42 @@
-it('whould', () => {
-  var res = 2;
-  throw new Error('could not tets');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHTTP = require('chai-http');
+const httpMocks = require('node-mocks-http');
+const app = require('../index');
+const jwt = require('jsonwebtoken');
+const loginController = require('../controllers/login');
+chai.use(chaiHTTP);
+
+describe('Login Suite', function() {
+  it('Should generate a valid token', done => {
+    let req = httpMocks.createRequest();
+    let res = httpMocks.createResponse();
+    req.body.fb = true;
+    req.body.id = 1234;
+    loginController.generateToken(req, res, function(err) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Should authorize a new token', done => {
+    let req = httpMocks.createRequest();
+    let res = httpMocks.createResponse();
+    const token =
+      'Bearer ' +
+      jwt.sign(
+        {
+          id: 1234
+        },
+        process.env.jwtSecret,
+        {
+          expiresIn: 40
+        }
+      );
+    req.headers.authorization = token;
+    loginController.authorize(req, res, function(err, next) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
 });
-//nodemon --exec "npm test"
