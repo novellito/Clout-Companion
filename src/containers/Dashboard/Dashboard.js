8d6473ce630dc0c42f9a3de2f2ad11f46a209904
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-06-26 20:22:53 -0700

    format scss files

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../store/actions/actionCreators';
-import AppNavbar from '../../components/AppNavbar/AppNavbar'
+import AppNavbar from '../../components/AppNavbar/AppNavbar';
 import './Dashboard.css';
 
 import axios from 'axios';
@@ -11,13 +11,11 @@ export class Dashboard extends Component {
     if (localStorage.length === 0) {
       this.props.history.replace('/');
     } else {
-
       // user is accessing dashboard via the token in storage
       // TODO: == get userinfo and store to redux state (later on)
       if (!this.props.isLog) {
         this.relog();
       }
-
     }
   }
 
@@ -27,54 +25,55 @@ export class Dashboard extends Component {
       Authorization: `Bearer ${localStorage.getItem('jwt')}`,
       'Content-Type': 'application/json'
     };
-    axios.post(
-      'http://localhost:5000/api/login/authorize',
-      null
-      ,
-      { headers }
-    )
-      .then((res) => console.log(res))
-      .catch(err => {// The token is invalid - make the user login again
+    axios
+      .post('http://localhost:5000/api/login/authorize', null, { headers })
+      .then(res => console.log(res))
+      .catch(err => {
+        // The token is invalid - make the user login again
         this.props.onRelog();
         this.props.history.replace('/login');
       });
-  }
+  };
 
   render() {
     return (
       <div>
         <AppNavbar history={this.props.history} />
-        <h1>Welcome to the dash!</h1>
-        <button onClick={this.relog} className="button">
-          Log out
-        </button>
-        <div className="">
-          <div className="wrapper">
-            <div className="box graph">
-              <div className="card blue-grey darken-1">
-                <div className="card-content white-text">
-                  <span className="card-title">Graph</span>
-                  <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-                </div>
+        <h1 className="comp-title dash">Dashboard</h1>
+        <div className="wrapper">
+          <div className="box graph">
+            <div className="card dash-card">
+              <div className="card-content white-text">
+                <span className="card-title">Graph</span>
+                <p>
+                  I am a very simple card. I am good at containing small bits of
+                  information. I am convenient because I require little markup
+                  to use effectively.
+                </p>
               </div>
             </div>
-            <div className="box item-list">
-              <div className="card blue-grey darken-1">
-                <div className="card-content white-text">
-                  <span className="card-title"> Items List</span>
-                  <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-                </div>
+          </div>
+          <div className="box item-list">
+            <div className="card dash-card">
+              <div className="card-content white-text">
+                <span className="card-title"> Items List</span>
+                <p>
+                  I am a very simple card. I am good at containing small bits of
+                  information. I am convenient because I require little markup
+                  to use effectively.
+                </p>
               </div>
             </div>
-            <div className="box notes">
-              <div className="card  blue-grey darken-1">
-                <div className="card-content white-text">
-                  <span className="card-title">Notes</span>
-                  <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-                </div>
+          </div>
+          <div className="box notes">
+            <div className="card  dash-card">
+              <div className="card-content white-text">
+                <span className="card-title">Notes</span>
+                <p>
+                  I am a very simple card. I am good at containing small bits of
+                  information. I am convenient because I require little markup
+                  to use effectively.
+                </p>
               </div>
             </div>
           </div>
