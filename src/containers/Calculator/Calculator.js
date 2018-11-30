52074bcc9c4783a3db377244506ab669a8e8236e
novellito <novellito7@gmail.com>
2018-11-29 22:48:35 -0500

    start initial calculator component refactor   -Add number formatter   -commented out deprecated code

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Collapsible, CollapsibleItem } from 'react-materialize';
 import RowContainer from '../../hoc/rowContainer';
+import NumberFormat from 'react-number-format';
 import {
   calculatePaypal,
   calculateGrailed,
@@ -33,42 +34,43 @@ class Calculator extends Component {
   setValue = e => {
     let calculation = null;
     // Determine which calculation to make
-    const parsedValue = e.target.value.replace(/^0+/, '').replace(/\s+/g, ''); // remove leading 0's and extra spaces
+    const parsedValue = e.target.value;
+    // const parsedValue = e.target.value.replace(/^0+/, '').replace(/\s+/g, ''); // remove leading 0's and extra spaces
     const validate = this.verifyValueInput(parsedValue);
 
     // invalid character input
-    if (validate === -1) {
-      this.setState({
-        [e.target.name + 'Result']: {
-          ...this.state[e.target.name + 'Result'],
-          status: -1
-        }
-      });
-    } else if (validate === 0) {
-      //user entered 0
-      this.setState({
-        [e.target.name + 'Result']: {
-          ...this.state[e.target.name + 'Result'],
-          status: 0
-        }
-      });
-    } else {
-      // do appropriate calculation
-      switch (e.target.name) {
-        case 'Paypal':
-          calculation = calculatePaypal(parsedValue);
-          break;
-        case 'Grailed':
-          calculation = calculateGrailed(parsedValue);
-          break;
-        default:
-          calculation = calculateStockx(parsedValue, this.state.StockxRate);
-      }
-      this.setState({
-        [e.target.name]: parsedValue,
-        [e.target.name + 'Result']: calculation
-      });
+    // if (validate === -1) {
+    //   this.setState({
+    //     [e.target.name + 'Result']: {
+    //       ...this.state[e.target.name + 'Result'],
+    //       status: -1
+    //     }
+    //   });
+    // } else if (validate === 0) {
+    //   //user entered 0
+    //   this.setState({
+    //     [e.target.name + 'Result']: {
+    //       ...this.state[e.target.name + 'Result'],
+    //       status: 0
+    //     }
+    //   });
+    // } else {
+    // do appropriate calculation
+    switch (e.target.name) {
+      case 'Paypal':
+        calculation = calculatePaypal(parsedValue);
+        break;
+      case 'Grailed':
+        calculation = calculateGrailed(parsedValue);
+        break;
+      default:
+        calculation = calculateStockx(parsedValue, this.state.StockxRate);
     }
+    this.setState({
+      [e.target.name]: parsedValue,
+      [e.target.name + 'Result']: calculation
+    });
+    // }s
 
     if (parsedValue === '' || parsedValue === null) {
       let labelRef = this.state.shippingField[e.target.name]; // create this so state is not mutated directly
